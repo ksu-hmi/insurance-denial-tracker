5a08c3c29ab6a1932f021e89362c6b2ff0c706ba
kensantanu <31720797+kensantanu@users.noreply.github.com>
2023-12-21 16:35:06 -0800

    Add Gradio UI

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import gradio as gr
+from pymongo import MongoClient
+from datetime import datetime
+
+with gr.Blocks() as ui:
+    with gr.Tab("Record"):
+        with gr.Row():
+            ln = gr.Dropdown(label="Last Name")
+            fn = gr.Dropdown(label="First Name")
+            dob = gr.Dropdown(label="Date of Birth")
+            find_btn = gr.Button("Find")
+        notes = gr.TextArea(label="Notes")
+        record_submit_btn = gr.Button("Submit")
+    with gr.Tab("Report"):
+        with gr.Row():
+            filter = gr.Dropdown(label="Filter", choices=["Last Name", "First Name", "Date of Birth", "Date of Service", "Bill Amount", "Paid?"])
+            condition = gr.Dropdown(label="Condition", choices=["Equals", "Contains"])
+            value = gr.Textbox (label="Value")
+            filter_btn = gr.Button("Filter")
+        out = gr.TextArea(label="Results")
+    with gr.Tab("Settings"):
+        gr.Markdown(
+        """
+        # Input New Patient
+        """)
+        with gr.Row():            
+            ln = gr.Textbox (label="Last Name")
+            fn = gr.Textbox (label="First Name")
+            dob = gr.Textbox (label="Date of Birth")
+        settings_submit_btn = gr.Button("Submit")
+
+ui.launch(server_name='0.0.0.0')
