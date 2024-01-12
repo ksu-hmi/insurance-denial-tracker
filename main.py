697e1f2f440d9cd51825df170210c16d831cf30b
kensantanu <31720797+kensantanu@users.noreply.github.com>
2024-01-12 12:13:25 -0800

    Move add new patient fucntion form setting tab to record tab

--- main.py
+++ main.py
@@ -250,8 +250,15 @@ with gr.Blocks(title="Denials Tracker", analytics_enabled=False) as ui:
     username_label = gr.Markdown("Logged in as: Guest")
     with gr.Tab("Record"):
         with gr.Row():
-            record_patientSelected_dropdown = gr.Dropdown(label="Patient List", choices=get_patients())
+            record_patientSelected_dropdown = gr.Dropdown(label="Patient List", choices=get_patients(), scale=5)
+            record_patientAdd_btn = gr.Button("Add New Patient", visible=False)
             record_patientRefresh_btn = gr.Button("Refresh")
+        with gr.Row(visible=False) as record_addNewPt_row:            
+            record_addNewPt_lastName = gr.Textbox(label="Last Name")
+            record_addNewPt_firstName = gr.Textbox(label="First Name")
+            record_addNewPt_dob = gr.Textbox(label="Date of Birth")
+            record_addNewPt_addPatient_btn = gr.Button("Add Patient")
+            record_addNewPt_cancel_btn = gr.Button("Cancel")
         with gr.Column() as record_patient_grp:
             with gr.Accordion(label= "Input new note", visible=False, open=False) as record_input_accordion:
                 with gr.Row():
@@ -293,20 +300,24 @@ with gr.Blocks(title="Denials Tracker", analytics_enabled=False) as ui:
                 settings_manageUser_createUser_btn = gr.Button("Create User")
             with gr.Row():
                 settings_manageUser_label = gr.Markdown()
-        with gr.Column(visible=False) as settings_addNewPt_grp:
-            with gr.Row():            
-                settings_lastName = gr.Textbox(label="Last Name")
-                settings_firstName = gr.Textbox(label="First Name")
-                settings_dob = gr.Textbox(label="Date of Birth")
-                setting_addNewPt_submit_btn = gr.Button("Submit")
-            setting_addNewPt_label = gr.Markdown(label="Output")
     
     # Event Handlers
     record_patientSelected_dropdown.select(
         fn = select_patient, inputs = [record_patientSelected_dropdown, session_state], outputs = session_state).then(
-        fn = list_denials, inputs = session_state, outputs = noteList)
+        fn = list_denials, inputs = session_state, outputs = noteList)    
+    record_patientAdd_btn.click(
+        fn = lambda: [gr.Textbox(value=""), gr.Textbox(value=""), gr.Textbox(value="")], outputs = [record_addNewPt_lastName, record_addNewPt_firstName, record_addNewPt_dob]).then(
+        fn = lambda: gr.Row(visible=True), outputs = record_addNewPt_row)
     record_patientRefresh_btn.click(
         fn = lambda: gr.Dropdown(choices=get_patients()), outputs = record_patientSelected_dropdown)
+    
+    record_addNewPt_addPatient_btn.click(
+        fn = add_patient, inputs = [record_addNewPt_lastName, record_addNewPt_firstName, record_addNewPt_dob]).success(
+        fn = lambda: gr.Row(visible=False), outputs = record_addNewPt_row).then(
+        fn = lambda: gr.Dropdown(choices=get_patients()), outputs = record_patientSelected_dropdown)
+    record_addNewPt_cancel_btn.click(
+        fn = lambda: gr.Row(visible=False), outputs = record_addNewPt_row)
+    
     record_status.change(fn = lambda x: gr.Textbox(visible=True) if x == "Paid" else gr.Textbox(visible=False), inputs = record_status, outputs = record_paidAmt)
     record_submit_btn.click(fn = set_denial, inputs = [record_dos, record_billAmt, record_status, record_paidAmt, record_note, session_state], outputs = record_inputNote_label).then(
         fn = lambda: gr.Textbox(value=""), outputs = record_billAmt).then(
@@ -316,14 +327,14 @@ with gr.Blocks(title="Denials Tracker", analytics_enabled=False) as ui:
         fn = gather_report_state, inputs = [report_filter, report_filter_condition, report_filter_value, report_sort, report_sort_condition], outputs = report_state).then(
         fn = get_report, inputs = report_state, outputs = report_list)
     
-    settings_optionList_dropdown.select(fn = settings_options, inputs = settings_optionList_dropdown, outputs = [settings_login_grp, settings_addNewPt_grp])
+    settings_optionList_dropdown.select(fn = settings_options, inputs = settings_optionList_dropdown, outputs = [settings_login_grp])
     settings_login_login_btn.click(
         fn = authenticate, inputs = [settings_login_username, session_state], outputs = [settings_login_label, session_state]).then(
+        fn = lambda x: gr.Button(visible=True) if x['user'] != "Guest" else gr.Button(visible=False), inputs = session_state, outputs = record_patientAdd_btn).then(
         fn = lambda x: gr.Accordion(visible=True) if x['user'] != "Guest" else gr.Accordion(visible=False), inputs = session_state, outputs = record_input_accordion).then(
-        fn = lambda: gr.Dropdown(choices=["Login", "Add New Patient"]), outputs = settings_optionList_dropdown).then(
+        fn = lambda: gr.Dropdown(choices=["Login"]), outputs = settings_optionList_dropdown).then(
         fn = update_username, inputs = session_state, outputs = username_label)
     settings_manageUser_createUser_btn.click(fn = set_user, inputs = [settings_manageUser_username, settings_manageUser_password], outputs = settings_manageUser_label)
-    setting_addNewPt_submit_btn.click(fn = add_patient, inputs = [settings_lastName, settings_firstName, settings_dob], outputs = setting_addNewPt_label)
 
 # Run Gradio server
 ui.launch(server_name='0.0.0.0', show_api=False)
